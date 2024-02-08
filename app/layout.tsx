70dfbae4f69f41cdd9ebcddd97d170bab4efd201
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-02-08 23:33:53 +0530

    vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
